2335b7a64e7b187b39d8ae1814df2c89e2148973
SanidhyaMalviya <72500621+SanidhyaMalviya@users.noreply.github.com>
2021-03-26 13:50:38 +0530

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -4,15 +4,17 @@ class Umbrella{
             isStatic:true
         }
 
-        this.image = loadImage("images/walking_1.png");
+        this.loadImg = loadImage("images/walking.gif");
+        this.createImg = createImg("images/walking.gif");
         this.body = Bodies.circle(x,y,50,option);
         this.radius = 50;
         World.add(world,this.body);
     }
 
     display(){
-        var pos = this.body.position
-        imageMode(CENTER)
-        image(this.image,pos.x,pos.y+70,300,300);
+        var pos = this.body.position;
+        // imageMode(CENTER);
+        // image(this.image,pos.x,pos.y+70,300,300);
+        this.createImg.position(pos.x-155,pos.y-80);
     }
 }
